bb1ceebec8b4de40f32243255c54f7b4783221c0
Wiseley Wu <wiseleywu@gmail.com>
2015-10-09 00:46:09 -0700

    Add functions: 	deleteMatches() 	deletePlayers() 	countPlayers() 	registerPlayer(name)

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
 import psycopg2
-
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
@@ -13,26 +12,55 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
-
+    db = psycopg2.connect("dbname=tournament")
+    c = db.cursor()
+    query = '''
+    delete from matches;
+    '''
+    c.execute(query)
+    db.commit()
+    db.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
-
+    db = psycopg2.connect("dbname=tournament")
+    c = db.cursor()
+    query = '''
+    delete from players;
+    '''
+    c.execute(query)
+    db.commit()
+    db.close()
 
 def countPlayers():
     """Returns the number of players currently registered."""
-
+    db = psycopg2.connect("dbname=tournament")
+    c = db.cursor()
+    query = '''
+    select count(*) as total from players;
+    '''
+    c.execute(query)
+    count = c.fetchone()
+    db.close()
+    return count[0]
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
-
+    db = psycopg2.connect("dbname=tournament")
+    c = db.cursor()
+    query = '''
+    insert into players (player_id, name) values (DEFAULT, %s);
+    '''
+    c.execute(query, (name,))
+    db.commit()
+    db.close()
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
@@ -56,16 +84,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +101,3 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
