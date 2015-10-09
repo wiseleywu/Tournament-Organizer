0d54bea8e98fafe57a09c0f567df3d0411b06123
Wiseley Wu <wiseleywu@gmail.com>
2015-10-09 00:44:20 -0700

    Update tournament.sql to create database, table players, and table matches. Drop database at the beginning before create database

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,10 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
-
+drop database IF EXISTS tournament;
+create database tournament;
+\c tournament;
+create table players(player_id serial primary key, name text);
+create table matches(match_num integer,
+                     player_id integer references players,
+                     win_lose integer);
