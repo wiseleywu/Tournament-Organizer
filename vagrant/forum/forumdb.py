e5ced2f38b6d4931d4b6c08da19dddbaa90f8f97
Wiseley Wu <wiseleywu@gmail.com>
2015-10-10 02:08:42 -0700

    Modify forumdb.py to store data in a relational database

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,9 +1,10 @@
 #
 # Database access functions for the web forum.
-# 
+#
 
 import time
-
+import psycopg2
+import bleach
 ## Database connection
 DB = []
 
@@ -16,8 +17,16 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+#    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
+#    posts.sort(key=lambda row: row['time'], reverse=True)
+#    return posts
+    forum = psycopg2.connect("dbname=forum")
+    c = forum.cursor()
+    query = "select time, content from posts order by time desc;"
+    c.execute(query)
+    posts = ({'content':str(bleach.clean(row[1])), 'time':str(row[0])}
+             for row in c.fetchall())
+    forum.close()
     return posts
 
 ## Add a post to the database.
@@ -27,5 +36,10 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+#    t = time.strftime('%c', time.localtime())
+#    DB.append((t, content))
+    forum = psycopg2.connect("dbname=forum")
+    c = forum.cursor()
+    c.execute("insert into posts (content) values (%s)", (bleach.clean(content, strip=True),))
+    forum.commit()
+    forum.close()
